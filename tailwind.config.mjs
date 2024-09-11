601a0376bcb5c616080a3ed501b83142d02b1a4d
Kumar Deshmukh <kumar.deshmukh945@gmail.com>
2024-09-11 12:35:17 +0530

    Responsive navbar added

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,11 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    fontFamily: {
+      serif: ["Crimson Text", "ui-serif"],
+      sans: ["Nunito Sans", "ui-sans-serif", "system-ui"],
+      mono: ["IBM Plex Mono", "ui-monospace"],
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/typography")],
