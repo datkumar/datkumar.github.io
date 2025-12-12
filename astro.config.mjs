fc41208e50f72abcc839f43fb081dcf159d077c3
Kumar Deshmukh <kumar.deshmukh945@gmail.com>
2025-12-12 21:05:25 +0530

    WIP: local changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  server: {
+    host: true,
+  },
   integrations: [],
   site: "https://datkumar.github.io",
 
